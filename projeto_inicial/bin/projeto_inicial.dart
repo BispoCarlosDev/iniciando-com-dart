28fc43db873d47623663c505936335f6099d23ca
Carlos <carlos.oliveirabispo@hotmail.com>
2024-12-16 09:54:32 -0400

    Comentando o código

--- projeto_inicial/bin/projeto_inicial.dart
+++ projeto_inicial/bin/projeto_inicial.dart
@@ -1,5 +1,6 @@
 void main() {
 
+  //Declarando as Variáveis do Projeto
   int idade = 23;
   double altura = 1.70;
   bool geek = true;
@@ -8,28 +9,34 @@ void main() {
   bool maiorDeIdade;
   int energia = 100;
 
+  //Verificando se a Persona é maior de Idade
   if(idade >= 18){
     maiorDeIdade = true;
   }else{
     maiorDeIdade = false;
   }
 
+  //Criando um Laço For que conta as atividades Entregues na Faculdade
   /*for(int i=0; i<10; i++){
     print('Entreguei $i atividades na Faculdade.');
   }*/
 
+  //Criando um Laço While que mostra a Energia que eu fico após realizar cada atividade
   /*while(energia > 0){
     energia = energia - 12;
     print('Tarefa Entregue. Fiquei com $energia% de energia.');
   }*/
 
+  //Criando um Laço Do While que mostra a Energia gasta nas atividades
   do{
     energia -= 12;
     print('Tarefa Realizada! Fiquei com $energia% de Energia.');
   }while(energia > 0);
 
+  //Criando uma Lista Dinâmica para armazenar os dados de uma Persona
   List<dynamic> gentil = [idade, altura, geek, nome, apelido, maiorDeIdade];
 
+  //String que utiliza os itens da Lista
   String frase = 'Eu sou o ${gentil[3]}.'
   '\nTodos me chamam de ${gentil[4]}.'
   '\nEu me considero geek? ${gentil[2]}.'
@@ -38,6 +45,7 @@ void main() {
   '\nEu Sou Maior de Idade? ${gentil[5]}.';
 
   //List<String> listanomes = ['Pedro', 'João', 'Moacir', 'Withor', 'Stephanie'];
+  //TODO: Esta é uma Lista com os nomes dos meu colegas para criar outras personas futuramente
 
   print(frase);
 
