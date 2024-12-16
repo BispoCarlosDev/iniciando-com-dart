0a9294b94bb80ef6cbb89d78e63f8bcdc7f0fc78
Carlos <carlos.oliveirabispo@hotmail.com>
2024-12-16 00:41:12 -0400

    const, final e var

--- projeto_inicial/bin/projeto_inicial.dart
+++ projeto_inicial/bin/projeto_inicial.dart
@@ -1,12 +1,12 @@
 void main() {
 
   int idade = 23;
-  double altura = 1.70;
-  bool geek = (idade == altura);
-  String nome = 'Carlos';
-  String apelido = 'Gentil';
+  double altura = 1.75;
+  bool geek = true;
+  const String nome = 'Carlos Eduardo de Oliveira Bispo';
+  final String apelido = 'Gentil';
 
-  List<dynamic> gentil = [23, 1.70, true, 'Carlos Bispo', 'Gentil'];
+  List<dynamic> gentil = [idade, altura, geek, nome, apelido];
 
   String frase = 'Eu sou o ${gentil[3]}, \ntodos me chamam de ${gentil[4]}. \nEu me '
   'considero geek? ${gentil[2]}, \ntenho ${gentil[1]} de altura, \ntenho ${gentil[0]} anos.';
