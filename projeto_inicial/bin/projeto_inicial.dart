57b837cdf6cec939cc6be1dbcccd4ccbdae27dbe
Carlos <carlos.oliveirabispo@hotmail.com>
2024-12-16 09:01:50 -0400

    Laço For

--- projeto_inicial/bin/projeto_inicial.dart
+++ projeto_inicial/bin/projeto_inicial.dart
@@ -1,7 +1,7 @@
 void main() {
 
   int idade = 23;
-  double altura = 1.75;
+  double altura = 1.70;
   bool geek = true;
   const String nome = 'Carlos Eduardo de Oliveira Bispo';
   final String apelido = 'Gentil';
@@ -13,13 +13,17 @@ void main() {
     maiorDeIdade = false;
   }
 
+  for(int i=0; i<10; i++){
+    print('Entreguei $i atividades na Faculdade.');
+  }
+
   List<dynamic> gentil = [idade, altura, geek, nome, apelido, maiorDeIdade];
 
   String frase = 'Eu sou o ${gentil[3]}.'
-  '\ntodos me chamam de ${gentil[4]}.'
+  '\nTodos me chamam de ${gentil[4]}.'
   '\nEu me considero geek? ${gentil[2]}.'
-  '\ntenho ${gentil[1]} de altura.'
-  '\ntenho ${gentil[0]} anos.'
+  '\nTenho ${gentil[1]} de altura.'
+  '\nTenho ${gentil[0]} anos.'
   '\nEu Sou Maior de Idade? ${gentil[5]}.';
 
   //List<String> listanomes = ['Pedro', 'João', 'Moacir', 'Withor', 'Stephanie'];
