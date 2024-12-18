f7d6296e8d1be0f6d3c6be7a21876b7cf0abf9e6
Carlos <carlos.oliveirabispo@hotmail.com>
2024-12-18 15:11:55 -0400

    utilizando enum

--- dart_curso_tres/bin/dart_curso_tres.dart
+++ dart_curso_tres/bin/dart_curso_tres.dart
@@ -1,5 +1,38 @@
-import 'package:dart_curso_tres/dart_curso_tres.dart' as dart_curso_tres;
+void main() {
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_curso_tres.calculate()}!');
+  //escolherMeioTransporte(50);
+  escolherMeioTransporteEnum(Transporte.Bike);
+
+}
+
+void escolherMeioTransporte(int locomocao){
+  if(locomocao == 0){
+    print('Vou de Carro para a Aventura!');
+  }else if(locomocao == 1){
+    print('Vou de Bike para a Aventura!');
+  }else{
+    print('Vou a pé mesmo, mas não perco a Aventura!');
+  }
+}
+
+void escolherMeioTransporteEnum(Transporte locomocao){
+  if(locomocao == Transporte.Carro){
+    print('Vou de Carro para a Aventura!');
+  }else if(locomocao == Transporte.Lancha){
+    print('Vou de Lancha para a Aventura!');
+  }else{
+    print('Vou a pé mesmo, mas não perco a Aventura!');
+  }
+}
+
+
+enum Transporte{
+  Carro,
+  Bike,
+  Andando,
+  Correndo,
+  Moto,
+  Aviao,
+  Lancha,
+  JetSki,
 }
