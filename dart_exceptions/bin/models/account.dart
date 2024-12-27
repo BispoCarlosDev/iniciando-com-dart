0b8efa8069f17648e7fe1d4b0af6a804749a84aa
Carlos <carlos.oliveirabispo@hotmail.com>
2024-12-27 10:13:08 -0400

    entendendo asserts

--- dart_exceptions/bin/models/account.dart
+++ dart_exceptions/bin/models/account.dart
@@ -6,7 +6,9 @@ class Account {
   Account(
       {required this.name,
       required this.balance,
-      required this.isAuthenticated});
+      required this.isAuthenticated}):
+  assert(name.isNotEmpty, 'O nome tem que ser preenchido.'),
+  assert(balance > 0, 'O valor não pode ser negativo.');
 
   editBalance({required value}) {
     balance = balance + value;
