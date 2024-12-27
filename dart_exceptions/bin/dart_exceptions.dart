11f4b82f11353320d317c0411b5fd69e5210c217
Carlos <carlos.oliveirabispo@hotmail.com>
2024-12-26 20:15:29 -0400

    arquivo para testar excessoes

--- dart_exceptions/bin/dart_exceptions.dart
+++ dart_exceptions/bin/dart_exceptions.dart
@@ -1,5 +1,19 @@
-import 'package:dart_exceptions/dart_exceptions.dart' as dart_exceptions;
+void main() {
+  print('Started Main!');
+  function01();
+  print('Finished Main!');
+}
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_exceptions.calculate()}!');
+void function01(){
+  print('Started F01!');
+  function02(1);
+  print('Finished F01!');
+}
+
+void function02(int number){
+  print('Started F02!');
+  for (int i = number; i <= 5; i++){
+    print(i);
+  }
+  print('Finished F02!');
 }
