fc3cc734dc5869ed9d34fe687d2a0089e4892e63
Carlos <carlos.oliveirabispo@hotmail.com>
2024-12-26 20:52:48 -0400

    Tratando Exceptions

--- dart_exceptions/bin/dart_exceptions.dart
+++ dart_exceptions/bin/dart_exceptions.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
   print('Started Main!');
   function01();
@@ -6,7 +8,28 @@ void main() {
 
 void function01(){
   print('Started F01!');
-  function02(1);
+  /*try{
+    function02(1);
+    } on FormatException{
+      print('Deu bug nessa function.');
+    }*/
+  try{
+    function02(1);
+    } on FormatException catch (e){
+      print('Deu bug nessa function.');
+      print(e.message);
+      print(e.source);
+      print(e.toString());
+    } on HttpException catch (e){
+      print('Uma HttpException foi encontrada.');
+      e.toString();
+    } on IOException catch (e){
+      print('Uma IOException foi encontrada.');
+      e.toString();
+    } on Exception catch (e){
+      print('Foi Detectada uma Excessão.');
+      print(e.toString());
+    }
   print('Finished F01!');
 }
 
@@ -14,6 +37,7 @@ void function02(int number){
   print('Started F02!');
   for (int i = number; i <= 5; i++){
     print(i);
+    double amount = double.parse('Not a Number');
   }
   print('Finished F02!');
 }
