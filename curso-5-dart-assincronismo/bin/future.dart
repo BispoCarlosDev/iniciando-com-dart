0102c9bff6791495321d47e29828c8ae651e044d
Carlos <carlos.oliveirabispo@hotmail.com>
2024-12-30 09:35:08 -0400

    tratando erros na function Future

--- curso-5-dart-assincronismo/bin/future.dart
+++ curso-5-dart-assincronismo/bin/future.dart
@@ -1,19 +1,42 @@
 import 'dart:io';
 void main(){
 
-  Future myFuture = Future((){
+  /*Future myFuture = Future((){
     print('Estamos indo de volta para o Futuro');
     print('Digite o Ano para onde você quer ir!');
     return stdin.readLineSync().toString();
-  }).then((onValue) => print('Estamos indo para $onValue'));
+  }).then((onValue) => print('Estamos indo para $onValue'));*/
 
   Future<int> myFutureFunc()async{
     print('Eu tenho uma função no Futuro.');
-    await Future.delayed(Duration(seconds: 15));
+    await Future.delayed(Duration(seconds: 5));
+    throw Exception();
     return 12;
   }
 
-  myFutureFunc().then((value) => print('O valor da minha função é: $value'));
+  /*myFutureFunc()
+  .then((value) => print('O valor da minha função é: $value'))
+  .onError((error, StackTrace) => print('OMG, aconteceu um erro.'))
+  .whenComplete(() => print('O Futuro foi Aniquilado!'));*/
+
+  Future<int> myFutureErrorFunc(int a, int b) async{
+    try{
+      if(a>b){
+      throw Exception();
+    }
+    print('Iniciando uma Nova Função');
+    await Future.delayed(Duration(seconds: 5));
+    return 42;
+    }catch(e){
+      print('Erro Ocorrido: $e');
+      return 42;
+    }finally{
+      print('O Futuro foi Aniquilado, Hasta La Vista Baby!');
+    }
+    
+  }
+
+  myFutureErrorFunc(5, 3).then((value) => print('O Valor é $value.'));
 
   print('Encerrando Main.');
 }
