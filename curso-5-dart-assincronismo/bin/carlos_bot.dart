d703fa0010ecfeea34fd389c23b35ad6ee4f6de0
Carlos <carlos.oliveirabispo@hotmail.com>
2024-12-30 09:47:28 -0400

    colocando tempo nas ações da aplicação

--- curso-5-dart-assincronismo/bin/carlos_bot.dart
+++ curso-5-dart-assincronismo/bin/carlos_bot.dart
@@ -1,7 +1,7 @@
 
 import 'dart:io';
-
 import 'questions/time_questions.dart';
+import 'timing/waiting_time.dart';
 
 void main() async {
   String carlosBot = 'CarlosBOT:\n';
@@ -9,11 +9,12 @@ void main() async {
   String usuario = '';
 
   print('-- Iniciando o CarlosBOT, aguarde..--');
-
+  await BotClock().clock(2);
   print('CarlosBOT:\n Oi :) \n Como posso ajudar?');
   do {
     usuario = stdin.readLineSync().toString();
     print('-- Processando pergunta, aguarde..--');
+    await BotClock().clock(2);
     if (usuario.contains('xau') ||
         usuario.contains('Xau') ||
         usuario.contains('Adeus') ||
@@ -22,10 +23,12 @@ void main() async {
       print(carlosBot + ' Até a proxima!!');
     } else if (TimeQuestions(usuario).isThisTime()) {
       // verificar antes, assim não fazemos toda a função sem precisar.
+      await BotClock().clock(3);
       TimeQuestions(usuario).timeQuestion();
     } else if (false) {
       //Basta adicionar novas perguntas aqui!
     } else {
+      await BotClock().clock(3);
       print(carlosBot +
           ' Não fui treinado para responder a essa pergunta \n Desculpe :( ');
       print(carlosBot + ' Você pode fazer outra pergunta ou dizer Adeus');
